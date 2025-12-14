51c6c02a16a11be65c9f575da4f5f48a2beae7f6
Marcos Dias <marcos.a.melo.dias@gmail.com>
2025-12-14 15:58:15 -0300

    Add external lib  to capture and parse the args valuess

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,16 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("USAGE: grrs [INPUT file]");
-    let path = std::env::args().nth(2).expect("USAGE: grrs [INPUT file]");
-
-    let args = Cli {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
 
     println!("pattern: {:?}, path {:?}", args.pattern, args.path);
 }
